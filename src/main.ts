c6700d4fc46e6d2b675311193ec947d3aa04cc35
ThaiNguyennd <thaittn01@gmail.com>
2025-06-19 14:35:15 +0700

    update upload file

--- src/main.ts
+++ src/main.ts
@@ -6,25 +6,31 @@ import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { TransformInterceptor } from './core/transform.interceptor';
 import cookieParser from 'cookie-parser';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express'; // cần để xài useStaticAssets
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // 👉 Cho phép FE truy cập ảnh trong thư mục /uploads
+  app.useStaticAssets(join(__dirname, '..', 'public'), {
+    prefix: '/public/',
+  });
+
   const configService = app.get(ConfigService);
-
   const reflector = app.get(Reflector);
-  app.useGlobalGuards(new JwtAuthGuard(reflector));
 
-  const PORT = configService.get<string>('PORT');
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor(reflector));
 
   app.enableCors({
     origin: '*',
   });
 
-  // config cookie
   app.use(cookieParser());
 
-  app.useGlobalInterceptors(new TransformInterceptor(reflector));
+  const PORT = configService.get<string>('PORT');
   await app.listen(PORT ?? 3000);
 }
 bootstrap();
